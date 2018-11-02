8e609d681e501f8a2216089e48bf56fb77afc8ed
Francois Kiesecoms <kiesecoms.francois@gmail.com>
2018-11-02 11:43:23 +0100

    models done

---
+++ app/models/dose.rb
@@ -0,0 +1,11 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates :description, presence: true
+  validates :ingredient, presence: true
+  validates :cocktail, presence: true, :uniqueness => {:scope => :ingredient}
+end
+
+
+# validates cokctail score ingredients
+
